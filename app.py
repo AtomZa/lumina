c94b7cfe78734ed32f417a70b5ffa504b300b95a
AtomZa <76245287+AtomZa@users.noreply.github.com>
2022-12-27 18:19:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy.signal import find_peaks
+import plotly.express as px
+import plotly.subplots as subplots
 
 import glob
 import  os
@@ -19,8 +21,12 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 # Navigation
 rad = st.sidebar.radio("Menu",["Preprocessing","Analysis","Result"])
 
-# Upload Files
-uploaded_files = st.sidebar.file_uploader("Choose a CSV file", accept_multiple_files=True)
+# Add a file uploader to the sidebar
+uploaded_folder = st.sidebar.text_input("Hi")
+
+# Print the uploaded folder
+if uploaded_folder is not None:
+    st.write(f"Selected folder: {uploaded_folder}")
 
 # Parameter 1
 st.sidebar.subheader("Smooth & Interpolate")
@@ -41,20 +47,47 @@ minwavelength = st.sidebar.number_input("Min Wavelength", min_value = 0, value =
 maxwavelength = st.sidebar.number_input("Max Wavelength", min_value = 0, value = 1000, step = 10)
 
 if rad == "Preprocessing":
-    st.title("Preprocessing")
-    # Raw Data
-    path = "testdata"
-    filelist = glob.glob(os.path.join(path, '*.csv'))
-    for idx, uploaded_file in enumerate(uploaded_files):
-        if idx < starttime: pass
-        elif idx > endtime: break
-        elif (idx-1) % timeinterval != 0: pass
+
+    # Check if a folder was uploaded
+    if uploaded_folder is None:
+        st.write("Please select a folder")
+    else:
+        # List all of the CSV files in the folder
+        filelist = glob.glob(os.path.join(uploaded_folder, '*.csv'))
+
+        # Create an empty list to store the plots
+        plots = []
+
+        for idx, uploaded_file in enumerate(filelist):
+            if idx < starttime:
+                pass
+            elif idx > endtime:
+                break
+            elif (idx-1) % timeinterval != 0:
+                pass
+            else:
+                df = pd.read_csv(uploaded_file, header=None)
+                df.columns = ["wavelength", "intensity"]
+                
+                # Use Plotly to create a line plot
+                fig = px.line(df, x="wavelength", y="intensity")
+                plots.append(fig)
+
+        # Check if the plots list is empty
+        if len(plots) == 0:
+            st.write("No plots to display")
         else:
-            df = pd.read_csv(uploaded_file, header=None)
-            df.columns = ["wavelength", "intensity"]
-            df["intensity"].plot()
-    st.pyplot()
-        
+            # Use the make_subplots function to combine all of the plots into a single figure
+            fig = subplots.make_subplots(rows=len(plots), cols=1)
+
+            # Add each plot to the figure as a separate trace
+            for plot in plots:
+                fig.add_traces(plot.data)
+
+            # Update the layout to adjust the size of the plot
+            fig.update_layout(width=800, height=3200)
+            st.plotly_chart(fig)
+            
 if rad == "Analysis": 
     st.title("Analysis")
     # Spectrum 
